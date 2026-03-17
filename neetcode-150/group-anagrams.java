4672bd7201d85e91723735caab6dd53e0420dd89
VilliDoug <143053997+VilliDoug@users.noreply.github.com>
2026-03-17 16:17:03 +0900

    Add solution to "Group Anagrams" problem

---
+++ neetcode-150/group-anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sorted = String.valueOf(chars);
+           
+           if(!map.containsKey(sorted)) {
+            map.put(sorted, new ArrayList<>());
+           }
+           map.get(sorted).add(s);
+        }
+        return new ArrayList<>(map.values());        
+        
+    }
+}
